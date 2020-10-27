5d059b9a4e009e262fa3fcef5f7c65387d2467a3
fuad-basarudin <57059540+fuad-basarudin@users.noreply.github.com>
2020-10-27 10:29:01 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://spons.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 100 * 1000);
