b20a4a08c166ff30cf3c18993a709ae67c71a756
fuad-basarudin <57059540+fuad-basarudin@users.noreply.github.com>
2020-11-26 20:58:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://fuad-dong.glitch.me"]
+const urls = ["https://reminder-1.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
